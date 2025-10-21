4634cf0ab1a4f883e40810355db31f4f066b70bb
ganesh2soft <ganesh2soft@hotmail.com>
2025-10-21 23:36:05 +0530

    First

--- ecom-heesho/eslint.config.js
+++ ecom-heesho/eslint.config.js
@@ -1,29 +1,43 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': 'warn',
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    extends: [
+      js.configs.recommended,
+      ...compat.extends('plugin:react/recommended'),
+      ...compat.extends('plugin:react-hooks/recommended'),
+      // react-refresh was removed due to incompatibility
+    ],
   },
 ])
